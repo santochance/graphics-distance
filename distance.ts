3f94730569cd8ca761c5301641b869e4e6789da6
Vincent <santochance@qq.com>
2022-10-10 16:23:49 +0800

    引入Skia CanvasKit

--- distance.ts
+++ distance.ts
@@ -1,4 +1,16 @@
 import { hitTest, parseCurves } from "./shapes/Path";
+// import CanvasKitInit from 'canvaskit-wasm/bin/canvaskit.wasm?init';
+import CanvasKitInit from "canvaskit-wasm/bin/canvaskit.js";
+import CanvasKitWasm from "canvaskit-wasm/bin/canvaskit.wasm?url";
+
+const CanvasKit = await CanvasKitInit({ locateFile: () => CanvasKitWasm });
+console.log('CanvasKit inited', CanvasKit);
+
+function createPath(elem) {
+  const pathD = elem.getAttribute('d');
+  const path = CanvasKit.Path.MakeFromSVGString(pathD);
+  return path;
+}
 
 function initStage() {
   console.log('[stage] inited');
@@ -7,14 +19,16 @@ function initStage() {
   // @ts-ignore
   const elems = Array.from(stage.querySelectorAll('path'));
   elems.forEach((elem) => {
-    const curves = parseCurves(elem.getAttribute('d'));
-    elem.curves = curves;
-    elem.dataset['curves'] = JSON.stringify(curves);
+    // const pathD = elem.getAttribute('d');
+    // const curves = parseCurves(pathD);
+    // elem.curves = curves;
+
+    elem.pathInst = createPath(elem);
   });
 
   const onMousemove = (ev) => {
-    const pos = getCurrentPosition(ev);
-    const hits = elems.filter((elem) => hitTest(elem, pos));
+    const pt = getCurrentPosition(ev);
+    const hits = searchElemsByPt(elems, pt);
     elems.forEach((el) => {
       el.classList.remove('is-hover');
     });
@@ -24,8 +38,8 @@ function initStage() {
   };
 
   const onMousedown = (ev) => {
-    const pos = getCurrentPosition(ev);
-    const hits = elems.filter((elem) => hitTest(elem, pos));
+    const pt = getCurrentPosition(ev);
+    const hits = searchElemsByPt(elems, pt);
     elems.forEach((el) => {
       el.classList.remove('is-hit');
     });
@@ -36,6 +50,11 @@ function initStage() {
 
   stage.addEventListener('mousemove', onMousemove);
   stage.addEventListener('mousedown', onMousedown);
+}
+
+function searchElemsByPt(elems, pt) {
+  // return elems.filter((elem) => hitTest(elem, pt));
+  return elems.filter((elem) => elem.pathInst.contains(pt.x, pt.y));
 }
 
 function getCurrentPosition(ev) {
